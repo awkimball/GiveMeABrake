33372f87afd9d525979786c5763a405d874d9cd0
TristanKnot <tristanknotts@gmail.com>
2018-11-05 18:41:54 -0600

    Add files via upload
    Updated routes defined. Including a zipcode table, updated vehicles, and shop owner

--- back/src/routes.php
+++ back/src/routes.php
@@ -47,7 +47,7 @@ $app->post('/register', function ($request, $response) {
     $sth->execute();
     return $this->response->withJson($input);
 });
-$app->put('/register/{username}', function ($request, $response) {
+$app->put('/account/{username}', function ($request, $response) {
 	$input = $request->getParsedBody();
 	$sql = "UPDATE users SET email = :email, username = :username, password = :password,
 			zipcode = : zipcode, account_type = :account_type WHERE idusers = :idusers";
@@ -65,27 +65,29 @@ $app->put('/register/{username}', function ($request, $response) {
 $app->post('/register_shop', function ($request, $response) {
     $input = $request->getParsedBody();
     $sql = "INSERT INTO 
-        shopowner (idusers, shop_email, shop_name, shop_zip, description) 
-        VALUES (:idusers, :shop_email, :shop_name, :shop_zip, :description)";
+        shopowner (idusers, gas_price,address, shop_email, shop_name, description) 
+        VALUES (:idusers, :gas_price,:address, :shop_email, :shop_name, :description)";
     $sth = $this->db->prepare($sql);
     $sth->bindParam("idusers", $input['idusers']);
+    $sth->bindParam("gas_price", $input['gas_price']);
+    $sth->bindParam("address", $input['address']);
     $sth->bindParam("shop_email", $input['shop_email']);
     $sth->bindParam("shop_name", $input['shop_name']);
-    $sth->bindParam("shop_zip", $input['shop_zip']);
     $sth->bindParam("description", $input['description']);
 
     $sth->execute();
     return $this->response->withJson($input);
 });
 
-$app->put('/register_shop/{idusers}', function ($request, $response) {
+$app->put('/account_shop/{idusers}', function ($request, $response) {
 	$input = $request->getParsedBody();
-	$sql = "UPDATE users SET shop_email = :shop_email, shop_name = :shop_name,
-			shop_zip = : shop_zip, description = :description WHERE idusers = :idusers AND shop_name = :shop_name";
+	$sql = "UPDATE users SET gas_price=:gas_price, address=:address, shop_email = :shop_email, shop_name = :shop_name,
+			 description = :description WHERE idusers = :idusers AND shop_name = :shop_name";
 	$sth = $this->db->prepare($sql);
+    $sth->bindParam("gas_price", $input['gas_price']);
+    $sth->bindParam("address", $input['address']);
     $sth->bindParam("shop_email", $input['shop_email']);
     $sth->bindParam("shop_name", $input['shop_name']);
-    $sth->bindParam("shop_zip", $input['shop_zip']);
     $sth->bindParam("description", $input['description']);
 
     $sth->execute();
@@ -114,17 +116,18 @@ $app->get('/vehicle', function ($request, $response, $args) {
 $app->post('/add_vehicle', function ($request, $response) {
     $input = $request->getParsedBody();
     $sql = "INSERT INTO 
-        vehicle (idusers, make, model, year, miles, oil_change_miles, tire_change_miles,
+        vehicle (idusers,vid, make, model, year, miles, tire_rotation_miles, tire_change_miles, 
         transmission_check_miles, last_inspection_date, general_description) 
-        VALUES (:idusers, :make, :model, :year, :miles, :oil_change_miles, :tire_change_miles,
+        VALUES (:idusers,:vid, :make, :model, :year, :miles, :tire_rotation_miles, :tire_change_miles,
         :transmission_check_miles, :last_inspection_date, :general_description)";
     $sth = $this->db->prepare($sql);
     $sth->bindParam("idusers", $input['idusers']);
+    $sth->bindParam("vid", $input['vid']);
     $sth->bindParam("make", $input['make']);
     $sth->bindParam("model", $input['model']);
     $sth->bindParam("year", $input['year']);
     $sth->bindParam("miles", $input['miles']);
-    $sth->bindParam("oil_change_miles", $input['oil_change_miles']);
+    $sth->bindParam("tire_rotation_miles", $input['tire_rotation_miles']);
     $sth->bindParam("tire_change_miles", $input['tire_change_miles']);
     $sth->bindParam("transmission_check_miles", $input['transmission_check_miles']);
     $sth->bindParam("last_inspection_date", $input['last_inspection_date']);
@@ -133,20 +136,21 @@ $app->post('/add_vehicle', function ($request, $response) {
     return $this->response->withJson($input);
 });
 
-$app->post('/add_vehicle/{idusers}', function ($request, $response) {
+$app->put('/add_vehicle/{idusers}', function ($request, $response) {
     $input = $request->getParsedBody();
     $sql = "UPDATE vehicle
-        SET idusers = :idusers, make=:make, model=:model, year=:year, miles=:miles, oil_change_miles=:oil_change_miles,
-        tire_change_miles=:tire_change_miles, transmission_check_miles=:transmission_check_miles,
+        SET idusers = :idusers, vid=:vid, make=:make, model=:model, year=:year, miles=:miles,
+        tire_rotation_miles=:tire_rotation_miles, tire_change_miles=:tire_change_miles transmission_check_miles=:transmission_check_miles,
         last_inspection_date=:last_inspection_date, general_description=:general_description WHERE
-        idusers = :idusers AND miles = :miles";
+        idusers = :idusers AND vid = :vid";
     $sth = $this->db->prepare($sql);
     $sth->bindParam("idusers", $input['idusers']);
+    $sth->bindParam("vid", $input['vid']);
     $sth->bindParam("make", $input['make']);
     $sth->bindParam("model", $input['model']);
     $sth->bindParam("year", $input['year']);
     $sth->bindParam("miles", $input['miles']);
-    $sth->bindParam("oil_change_miles", $input['oil_change_miles']);
+    $sth->bindParam("tire_rotation_miles", $input['tire_rotation_miles']);
     $sth->bindParam("tire_change_miles", $input['tire_change_miles']);
     $sth->bindParam("transmission_check_miles", $input['transmission_check_miles']);
     $sth->bindParam("last_inspection_date", $input['last_inspection_date']);
@@ -154,6 +158,58 @@ $app->post('/add_vehicle/{idusers}', function ($request, $response) {
     $sth->execute();
     return $this->response->withJson($input);
 });
+
+$app->get('/zip', function ($request, $response, $args) {
+    $sth = $this->db->prepare(
+        "SELECT * FROM zip WHERE zipcode=:zipcode"
+    );
+    $sth->bindParam("zipcode", $args['zipcode']);
+    $sth->execute();
+    $users = $sth->fetchAll();
+    return $this->response->withJson($users);
+});
+
+$app->post('/add_zip', function ($request, $response) {
+    $input = $request->getParsedBody();
+    $sql = "INSERT INTO 
+        zip (zipcode, longitude, latitude) 
+        VALUES (:zipcode, :longitude, :latitude)";
+    $sth = $this->db->prepare($sql);
+    $sth->bindParam("zipcode", $input['zipcode']);
+    $sth->bindParam("longitude", $input['longitude']);
+    $sth->bindParam("latitude", $input['latitude']);
+
+    $sth->execute();
+    return $this->response->withJson($input);
+});
+
+$app->get('/deals', function ($request, $response, $args) {
+    $sth = $this->db->prepare(
+        "SELECT * FROM deals WHERE idusers=:idusers"
+    );
+    $sth->bindParam("idusers", $args['idusers']);
+    $sth->execute();
+    $users = $sth->fetchAll();
+    return $this->response->withJson($users);
+});
+
+$app->put('/change_deal/{name}', function ($request, $response) {
+    $input = $request->getParsedBody();
+    $sql = "UPDATE deals
+    SET price=:price, name=:name, description=:description WHERE name=:name";
+    $sth = $this->db->prepare($sql);
+    $sth->bindParam("price", $input['price']);
+    $sth->bindParam("name", $input['name']);
+    $sth->bindParam("description", $input['description']);
+    $sth->execute();
+    return $this->response->withJson($input);
+});
+
+
+
+
+
+
 
 
 
