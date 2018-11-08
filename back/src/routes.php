cd8e95361782b3fa75618ece23ee5ee0daeca016
Addison Kimball <addison@akimball.com>
2018-11-08 11:25:51 -0600

    test

--- back/src/routes.php
+++ back/src/routes.php
@@ -29,6 +29,7 @@ $app->get('/login', function ($request, $response, $args) {
 
     $sth->execute();
     $users = $sth->fetchAll();
+    error_log($args);
     error_log($response);
     return $this->response->withJson($users);
 });
