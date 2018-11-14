3f6b97f6a0e7da80ae88ace02793933db4881189
TristanKnot <tristanknotts@gmail.com>
2018-11-14 16:37:13 -0600

    Zipcode search route
    Search for shops based on specific zipcode

--- back/src/routes.php
+++ back/src/routes.php
@@ -253,3 +253,13 @@ $app->get('/user/{idusers}/reviews', function ($request, $response, $args) {
     $users = $sth->fetchAll();
     return $this->response->withJson($users);
 });
+
+$app->get('/user/{zipcode}/shop', function ($request, $response, $args) {
+    $sth = $this->db->prepare(
+        "SELECT * FROM shopowner UNION users WHERE users.zipcode=:users.zipcode"
+    );
+    $sth->bindParam("zipcode", $args['zipcode']);
+    $sth->execute();
+    $users = $sth->fetchAll();
+    return $this->response->withJson($users);
+});
